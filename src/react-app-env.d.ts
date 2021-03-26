960f44b88770b82b55e079f12e1ddda4e6987505
Tomas Fiers <tomas.fiers@gmail.com>
2021-03-26 23:32:39 +0100

    ⬆IMPR add typescript

---
+++ src/react-app-env.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="react-scripts" />
+// Placed here by create-react-app on `npm start`.
+// Allows `import .svg` and `.css` in .tsx
+// Annoying.
+// https://github.com/facebook/create-react-app/issues/6560
