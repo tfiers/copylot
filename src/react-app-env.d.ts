78319b8090bcd4861fdeddc9826eb46f21c75ace
Tomas Fiers <tomas.fiers@gmail.com>
2021-03-27 16:38:09 +0100

    🔧MAINT remove: testing code & deps; manifest.json

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,5 +1,4 @@
 /// <reference types="react-scripts" />
 // Placed here by create-react-app on `npm start`.
 // Allows `import .svg` and `.css` in .tsx
-// Annoying.
 // https://github.com/facebook/create-react-app/issues/6560
